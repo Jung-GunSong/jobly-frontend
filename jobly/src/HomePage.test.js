193453a4e2e2f0df08814aa26dd4f1d3785a17cd
Jung-GunSong <jsong781@gmail.com>
2023-11-28 22:43:53 -0500

    Added testing for HomePage.js
    Completed tests for HomePage.js which include correct rendering of
    component for users and non-users. Also tested to see if login and
    signup button took user to login and sign up pages, respectively.

--- jobly/src/HomePage.test.js
+++ jobly/src/HomePage.test.js
@@ -1,19 +1,82 @@
-import { render,} from '@testing-library/react';
+import { fireEvent, render, screen} from '@testing-library/react';
 import HomePage from './HomePage';
+import { BrowserRouter } from 'react-router-dom';
 import userContext from './userContext';
+import RouteList from './RouteList';
+import NavBar from './NavBar';
+
+
 
 describe("HomePage", function () {
-  it("renders without crashing", function () {
+
+  function homePageRender(val){
     render(
-    <userContext.Provider value={{ username: "bob" }}>
-      <HomePage />
-    </userContext.Provider>);
+    <BrowserRouter>
+      <userContext.Provider value= {{username: val}}>
+        <HomePage />
+      </userContext.Provider>
+    </BrowserRouter>
+    );
+  }
+
+  it("renders when there is no user", function () {
+
+    homePageRender(null);
+
+    expect(screen.getByText("Jobly")).toBeInTheDocument();
+    expect(screen.queryByText("Welcome")).not.toBeInTheDocument();
   });
 
-  // it("matches snapshot", function (){
-  //   const {container} = render(<HomePage />)
+  it("renders when there is a user", function () {
 
-  //   expect(container).toMatchSnapshot();
-  // })
+    homePageRender("bob");
+
+    expect(screen.getByText("Jobly")).toBeInTheDocument();
+    expect(screen.getByText("Welcome back bob!")).toBeInTheDocument();
+  });
+
+  it("pressing login button takes user to login page", function () {
+
+    const { container } = render(
+    <BrowserRouter>
+      <userContext.Provider value= {{username: null}}>
+        <RouteList>
+        </RouteList>
+      </userContext.Provider>
+    </BrowserRouter>
+    );
+
+   fireEvent.click(container.querySelector(".login"));
+
+   expect(screen.getByText("Log In")).toBeInTheDocument();
+   expect(screen.getByText("Username:")).toBeInTheDocument();
+   expect(screen.getByText("Password:")).toBeInTheDocument();
+
+  });
+
+  it("pressing sign up button takes user to sign up page", function () {
+
+    const { container } = render(
+    <BrowserRouter>
+      <userContext.Provider value= {{username: null}}>
+        <NavBar />
+        <RouteList>
+        </RouteList>
+      </userContext.Provider>
+    </BrowserRouter>
+    );
+
+      fireEvent.click(container.querySelector(".homepage"));
+      fireEvent.click(container.querySelector(".signup"));
+
+   expect(screen.getByText("Sign Up")).toBeInTheDocument();
+   expect(screen.getByText("Username:")).toBeInTheDocument();
+   expect(screen.getByText("Password:")).toBeInTheDocument();
+   expect(screen.getByText("First Name:")).toBeInTheDocument();
+   expect(screen.getByText("Last Name:")).toBeInTheDocument();
+   expect(screen.getByText("Email:")).toBeInTheDocument();
+
+  });
+
 });
 
